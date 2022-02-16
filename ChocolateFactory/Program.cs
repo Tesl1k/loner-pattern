acbce73eb30f7777deff615c8c7c1e55451f089a
Tesl1k <danildavydov161@mail.ru>
2022-02-16 13:39:44 +0300

    добавлено

---
+++ ChocolateFactory/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChocolateLibrary;
+
+namespace ChocolateFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChocolateBoiler newBoiler = ChocolateBoiler.getInstance();
+
+            Console.WriteLine(newBoiler.Display());
+            Console.WriteLine(newBoiler.Fill());
+            Console.WriteLine(newBoiler.Boil());
+            Console.WriteLine(newBoiler.Drain());
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ChocolateLibrary/ChocolateBoiler.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChocolateLibrary
+{
+    public class ChocolateBoiler
+    {
+        bool empty;
+        bool boiled;
+        static string description;
+
+        public static ChocolateBoiler uniqueInstance;
+        private ChocolateBoiler()
+        {
+            empty = true;
+            boiled = false;
+        }
+
+        public static ChocolateBoiler getInstance()
+        {
+            if(uniqueInstance == null)
+            {
+                uniqueInstance = new ChocolateBoiler();
+                description = "Бойлер создан";
+            }
+
+            return uniqueInstance;
+        }
+        public string Fill()
+        {
+            if (isEmpty())
+            {
+                empty = false;
+                boiled = false;
+            }
+
+            return "Наполнено";
+        }
+        public string Drain()
+        {
+            if (!isEmpty() && isBoiled())
+            {
+                empty = true;
+            }
+
+            return "Слито";
+        }
+        public string Boil()
+        {
+            if (!isEmpty() && !isBoiled())
+            {
+                boiled = true;
+            }
+
+            return "Нагрето";
+        }
+        public bool isEmpty()
+        {
+            return empty;
+        }
+        public bool isBoiled()
+        {
+            return boiled;
+        }        
+
+        public string Display()
+        {
+            return description;
+        }
+    }
+}
